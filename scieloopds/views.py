c625dcc50e2d8f4339f61f7a9efecc97c3a65255
Allison Vollmann <allisonvoll@gmail.com>
2012-10-14 19:20:03 -0300

    ENT02 Publisher and alphabetical catalogs

--- scieloopds/views.py
+++ scieloopds/views.py
@@ -16,27 +16,88 @@ def root(request):
         '_id': 'http://books.scielo.org/opds/new', 
         'title' : 'New Releases', 
         'updated' : '2012-09-29T19:27:01Z',
-        'link' : {
+        'links' : [{
             'rel' : opds.NEW,
             'href' : '/opds/new',
-            'type' : opds.ACQUISITION}
+            'type' : opds.ACQUISITION}]
         })
     entry.append({
         '_id': 'http://books.scielo.org/opds/publishers',
         'title' : 'Publishers', 
         'updated' : '2012-09-29T19:27:01Z',
-        'link' : {
+        'links' : [{
             'rel' : opds.SUBSECTION,
             'href' : '/opds/publishers',
-            'type' : opds.NAVIGATION}
+            'type' : opds.NAVIGATION}]
         })
     entry.append({
         '_id': 'http://books.scielo.org/opds/alpha', 
         'title' : 'Alphabetical', 
         'updated' : '2012-09-29T19:27:01Z',
-        'link' : {
+        'links' : [{
             'rel' : opds.SUBSECTION,
             'href' : '/opds/alpha',
-            'type' : opds.NAVIGATION}
+            'type' : opds.NAVIGATION}]
         })
-    return {'entry': entry, 'link': link}
+    return {'entry': entry, 'links': link}
+
+
+@view_config(route_name='alpha_catalog', renderer='opds')
+def alpha_catalog(request):
+    """ OPDS Catalog Alpha /opds/alpha
+    """
+    link = []
+    link.append({
+        'rel': 'up',
+        'href': '/opds/',
+        'type': opds.NAVIGATION})
+    link.append({
+        'rel': 'self',
+        'href': '/opds/alpha',
+        'type': opds.NAVIGATION})
+
+    entry = [{
+        '_id': 'http://books.scielo.org/opds/alpha/a', 
+        'title': 'A',
+        'content': '10 titles',
+        'links': [{
+            'rel': opds.SUBSECTION,
+            'type': opds.ACQUISITION,
+            'href': '/opds/alpha/a'}]
+        }]
+
+    return {
+        '_id': 'http://books.scielo.org/opds/alpha',
+        'title': 'SciELO Books - Alphabetical',
+        'links': link,
+        'entry': entry}
+
+@view_config(route_name='publisher_catalog', renderer='opds')
+def publisher_catalog(request):
+    """ OPDS Catalog Publishers /opds/publisher
+    """
+    link = []
+    link.append({
+        'rel': 'up',
+        'href': '/opds/',
+        'type': opds.NAVIGATION})
+    link.append({
+        'rel': 'self',
+        'href': '/opds/publisher',
+        'type': opds.NAVIGATION})
+
+    entry = [{
+        '_id': 'http://books.scielo.org/opds/publisher/EDUFBA', 
+        'title': 'EDUFBA',
+        'content': '125 titles',
+        'links': [{
+            'rel': opds.SUBSECTION,
+            'type': opds.ACQUISITION,
+            'href': '/opds/publisher/EDUFBA'}]
+        }]
+
+    return {
+        '_id': 'http://books.scielo.org/opds/publisher',
+        'title': 'SciELO Books - Publishers',
+        'links': link,
+        'entry': entry}
