6562e7dd31a69066edd55b691548e01a4fb0d00e
Allison Vollmann <allisonvoll@gmail.com>
2012-10-17 04:06:52 -0300

    ENT04 Adicionado models para tratar Alphabetical, Publisher e Book

--- scieloopds/opds.py
+++ scieloopds/opds.py
@@ -1,16 +1,21 @@
 class ContentType:
     CATALOG = 'application/atom+xml;profile=opds-catalog'
-    NAVIGATION = u'application/atom+xml;profile=opds-catalog;kind=navigation'
-    ACQUISITION = u'application/atom+xml;profile=opds-catalog;kind=acquisition'
+    NAVIGATION = 'application/atom+xml;profile=opds-catalog;kind=navigation'
+    ACQUISITION = 'application/atom+xml;profile=opds-catalog;kind=acquisition'
 
 class LinkRel:
-    NEW = u'http://opds-spec.org/sort/new'
-    SUBSECTION = u'subsection'
-    ACQUISITION = u'http://opds-spec.org/acquisition'
-    ALTERNATE = u'alternate'
-    IMAGE = u'http://opds-spec.org/image'
-    THUMBNAIL = u'http://opds-spec.org/image/thumbnail'
+    UP = 'up'
+    SELF = 'self'
+    NEW = 'http://opds-spec.org/sort/new'
+    SUBSECTION = 'subsection'
+    ACQUISITION = 'http://opds-spec.org/acquisition'
+    ALTERNATE = 'alternate'
+    IMAGE = 'http://opds-spec.org/image'
+    THUMBNAIL = 'http://opds-spec.org/image/thumbnail'
 
 class Namespace:
-    OPDS = u'http://opds-spec.org/2010/catalog'
-    ATOM = u'http://www.w3.org/2005/Atom'
+    OPDS = 'http://opds-spec.org/2010/catalog'
+    ATOM = 'http://www.w3.org/2005/Atom'
+
+def make_link(rel, type, href):
+    return {'rel': rel, 'type': type, 'href': href}
