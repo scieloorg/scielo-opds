1535f990c67c7835129861dafff3d7caefd59e31
Allison Vollmann <allisonvoll@gmail.com>
2012-10-17 05:28:52 -0300

    ENT04 Adicionado views alpha_filter e publisher_filter

--- scieloopds/views.py
+++ scieloopds/views.py
@@ -17,11 +17,11 @@ def root(request):
             '/opds/new')]
         })
     entry.append({
-        '_id': 'http://books.scielo.org/opds/publishers',
+        '_id': 'http://books.scielo.org/opds/publisher',
         'title' : 'Publishers', 
         'updated' : time.strftime('%Y-%m-%dT%H:%M:%SZ', time.localtime()),
         'links' : [make_link(LinkRel.SUBSECTION, ContentType.NAVIGATION, 
-            '/opds/publishers')]
+            '/opds/publisher')]
         })
     entry.append({
         '_id': 'http://books.scielo.org/opds/alpha', 
@@ -52,6 +52,27 @@ def alpha_catalog(request):
         'links': link,
         'entry': entry}
 
+@view_config(route_name='alpha_filter', renderer='opds')
+def alpha_filter(request):
+    """ OPDS Alpha filter for books
+    """
+    alpha = request.matchdict['id']
+    link = [make_link('up', ContentType.NAVIGATION, '/opds/alpha'),
+        make_link('self', ContentType.NAVIGATION, 
+            '/opds/alpha/{}'.format(alpha))]
+
+    entry = []
+    for book in Book.filter(filter_initial=alpha):
+        book['links'] = [make_link(LinkRel.ALTERNATE, ContentType.ACQUISITION,
+            '/opds/book/{}'.format(book['_id']))]
+        entry.append(book)
+
+    return {
+        '_id': 'http://books.scielo.org/opds/alpha/{}'.format(alpha),
+        'title': 'SciELO Books - Filter starting with "{}"'.format(alpha),
+        'links': link,
+        'entry': entry}
+
 @view_config(route_name='publisher_catalog', renderer='opds')
 def publisher_catalog(request):
     """ OPDS Catalog Publishers /opds/publisher
@@ -70,6 +91,28 @@ def publisher_catalog(request):
         'title': 'SciELO Books - Publishers',
         'links': link,
         'entry': entry}
+
+@view_config(route_name='publisher_filter', renderer='opds')
+def publisher_filter(request):
+    """ OPDS Catalog Publisher filter for books
+    """
+    pub = request.matchdict['id']
+    link = [make_link('up', ContentType.NAVIGATION, '/opds/publisher'),
+        make_link('self', ContentType.NAVIGATION, 
+            '/opds/publisher/{}'.format(pub))]
+
+    entry = []
+    for book in Book.filter(filter_publisher=pub):
+        book['links'] = [make_link(LinkRel.ALTERNATE, ContentType.ACQUISITION,
+            '/opds/book/{}'.format(book['_id']))]
+        entry.append(book)
+
+    return {
+        '_id': 'http://books.scielo.org/opds/publisher/{}'.format(pub),
+        'title': 'SciELO Books - Filter for publisher "{}"'.format(pub),
+        'links': link,
+        'entry': entry}
+
 
 @view_config(route_name='new', renderer='opds')
 def new(request):
